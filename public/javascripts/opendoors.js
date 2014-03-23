b8fe9e4883119bfc33709c6993be0474db5c41ab
Greg Johnston <greg.johnston@gmail.com>
2014-03-22 21:13:15 -0400

    Titles by page

--- public/javascripts/opendoors.js
+++ public/javascripts/opendoors.js
@@ -27,44 +27,61 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider',
     $routeProvider
       .when('/people', {
         templateUrl: '/partials/person_list',
-        controller: "PersonListCtrl"
+        controller: "PersonListCtrl",
+        title: "People"
       })
       .when('/people/new', {
         templateUrl: '/partials/person_create',
-        controller: "PersonCreateCtrl"
+        controller: "PersonCreateCtrl",
+        title: "Add a Person"
       })
       .when('/people/:slug', {
         templateUrl: '/partials/person_detail',
-        controller: "PersonDetailCtrl"
+        controller: "PersonDetailCtrl",
+        title: "People"
       })
       .when('/people/:slug/edit', {
         templateUrl: '/partials/person_update',
-        controller: "PersonUpdateCtrl"
+        controller: "PersonUpdateCtrl",
+        title: "Edit a Person"
       })
       .when('/people/:slug/delete', {
         templateUrl: '/partials/person_delete',
-        controller: "PersonDeleteCtrl"
+        controller: "PersonDeleteCtrl",
+        title: "Delete a Person"
       })
       .when('/congregation', {
         templateUrl: '/partials/congregation_list',
-        controller: 'CongregationListCtrl'
+        controller: 'CongregationListCtrl',
+        title: "Congregations"
       })
       .when('/congregation/new', {
         templateUrl: '/partials/congregation_create',
-        controller: 'CongregationCreateCtrl'
+        controller: 'CongregationCreateCtrl',
+        title: "Add a Congregation"
       })
       .when('/:slug', {
         templateUrl: '/partials/congregation_detail',
-        controller: 'CongregationDetailCtrl'
+        controller: 'CongregationDetailCtrl',
+        title: "Congregations"
       })
       .when('/:slug/edit', {
         templateUrl: '/partials/congregation_update',
-        controller: 'CongregationUpdateCtrl'
+        controller: 'CongregationUpdateCtrl',
+        title: "Edit a Congregation"
       })
       .when('/:slug/delete', {
         templateUrl: '/partials/congregation_delete',
-        controller: 'CongregationDeleteCtrl'
+        controller: 'CongregationDeleteCtrl',
+        title: "Delete a Congregation"
       })
-      .otherwise({templateUrl: '/partials/404'});
+      .otherwise({templateUrl: '/partials/404', title: "Oh no!"});
     $locationProvider.html5Mode(true);
   }]);
+
+app.run(function($rootScope, $route) {
+  $rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute){
+      //Change page title, based on Route information
+      $rootScope.title = $route.current.title;
+    });
+});
